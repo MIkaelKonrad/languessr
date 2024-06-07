590e87fa4b733b2849edcfe34af3bfdfab6ff0fb
Mikael Konrad <mikael.k.8@gmail.com>
2024-06-07 11:26:14 +0200

    Added RecCheck

--- app.py
+++ app.py
@@ -84,9 +84,48 @@ def coordinate_ceck():
 
 @app.route('/doubleCheck', methods = ['POST','GET'])
 def double_check():
-    return render_template('doubleCheck.html')
+   
+        return render_template('doubleCheck.html')
 
 
+@app.route('/RecCheck', methods = ['POST','GET'])
+def RecCheck():
+    if request.method == 'POST':
+        country = request.form["Country"]
+        fam = request.form["fam"]
+        lang = request.form["lang"]
+        print(fam)
+        df = pd.read_csv('RecordingsList.csv')
+        if country != "":
+            df = df.loc[df['Country'] == country]
+        
+        if fam != "":
+            df = df.loc[df['Language Family'] == fam]
+        
+        if lang != "":
+            df = df.loc[df['Language'] == lang ]
+        table = '          <table class="table"> <tr> <th>Country</th>  <th>Language Family</th>  <th>Language</th>  <th>Latitude</th> <th>Longitude</th> <th class="wide-column">Recording</th> <th>Show Location</th> </tr>'
+        df= df.reset_index()
+        for index, row in df.iterrows():
+            next_row = "<tr> <td>"+row['Country'] +"</td> <td>"+row['Language Family'] +"</td> <td>"+ row['Language'] +"</td> <td>" +str(row['Longitude']) +"</td> <td>" + str(row['Latitude']) +"</td>"
+            rec = row['Recording']
+            rec_arr = rec.split('\\')
+            n = len(rec_arr)
+            rec_dir='/static'
+            i=6
+            while i<=n-1:
+                rec_dir =  rec_dir + '/' + rec_arr[i] 
+                i = i+1
+            recording = '<td> <audio controls> <source src='+rec_dir+' type="audio/mpeg"> Your browser does not support the audio element.</audio> </td>'
+            table = table + next_row + recording + ' <td> <Button>Location</Button></td></tr>'
+        table = table + " </table>"
+        df = df.to_csv(header=None, index=False).strip('\n').split('\n')
+        return render_template('RecCheck.html', Table = df, Table2 = table )
+    elif request.method == 'GET':
+        
+        return render_template('RecCheck.html', Table = '')
+    else :
+        return render_template('ReckCheck.html', Table = '')
 
 if __name__ == '__main__':
     app.run(debug=True)
